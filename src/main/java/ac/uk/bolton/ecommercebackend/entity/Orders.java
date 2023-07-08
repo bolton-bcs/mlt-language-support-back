53ef3ef3b0bf96e37d8427f50f77ac87e0e8c68a
danu <danujagreru@gmail.com>
2023-07-08 23:33:56 +0530

    order place with pending order

---
+++ src/main/java/ac/uk/bolton/ecommercebackend/entity/Orders.java
@@ -0,0 +1,46 @@
+package ac.uk.bolton.ecommercebackend.entity;
+
+/*
+    @author DanujaV
+    @created 7/8/23 - 8:21 PM   
+*/
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@DynamicInsert
+@Table(name = "orders")
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private Integer qty;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false, columnDefinition = "varchar(255)")
+    private String country;
+    @Column(name = "delivery_address", nullable = false, columnDefinition = "TEXT")
+    private String deliveryAddress;
+    @Column(name = "expected_date", nullable = false)
+    private Date expectedDate;
+    @Column(columnDefinition = "boolean default false")
+    private Boolean status;
+
+    public Orders(Integer qty, Double price, String country, String deliveryAddress, Date expectedDate) {
+        this.qty = qty;
+        this.price = price;
+        this.country = country;
+        this.deliveryAddress = deliveryAddress;
+        this.expectedDate = expectedDate;
+    }
+}
